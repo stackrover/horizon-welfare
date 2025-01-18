70ab7cc69aab6d0897cd9023d92472c28842cc04
MD Mehedi Hasan <mehedihasan.hr.324@gmail.com>
2025-01-18 18:24:19 +0600

    Update Home page content

--- src/app/actions/admin/pages/home.ts
+++ src/app/actions/admin/pages/home.ts
@@ -5,10 +5,7 @@ import { ERROR_OBJ_FORMAT } from "@/constants/error-obj-format";
 import { fetcher } from "@/lib/fetcher";
 import { revalidatePath, revalidateTag } from "next/cache";
 
-export async function saveHeroSectionData(
-  initialState: Record<string, string> | null,
-  formData: FormData,
-) {
+export async function saveHeroSectionData(formData: FormData) {
   const controller = new AbortController();
   const session = await auth();
 
@@ -60,6 +57,45 @@ export async function updateServiceSectionData(formData: FormData) {
 
   try {
     const res = await fetcher("/home/service/update", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${session?.user?.token}`,
+      },
+      body: formData,
+      signal: controller.signal,
+    });
+
+    const data = await res.json();
+
+    if (data.status === "success") {
+      revalidatePath("/admin/dashboard/pages/home", "page");
+      revalidateTag("homeSerivce");
+    }
+
+    return data;
+  } catch (error) {
+    return { ...ERROR_OBJ_FORMAT, error: error };
+  }
+}
+
+/******************************************/
+/****** UPDATE GALLERY SECTION ******/
+/******************************************/
+
+export async function updateGallerySectionData(formData: FormData) {
+  const controller = new AbortController();
+  const session = await auth();
+
+  // return error if user is not authenticated
+  if (!session?.user?.token) {
+    return { ...ERROR_OBJ_FORMAT, message: "Unauthorized" };
+  }
+
+  formData.append("updated_by", session?.user?.id as string);
+
+  try {
+    const res = await fetcher("/home/gallery/update", {
       method: "POST",
       headers: {
         Accept: "application/json",
