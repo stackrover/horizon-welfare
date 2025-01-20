202c621d1b13de1f8fb0307f332941c390f9342f
Joy Debnath <developerjoydebnath@gmail.com>
2025-01-20 23:15:13 +0600

    admin what we do kid section

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
 import { config } from "@/utils/config";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { config } from "@/utils/config";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -10,7 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 // Returns the full URL for an image or an empty string if no source is provided.
 export const getImageURL = (src?: string): string => {
   if (!src) return "";
-  
+
   // Remove leading slash if it exists
   const normalizedSrc = src.startsWith("/") ? src.slice(1) : src;
 
