ea99a7e967337573efa25558e42fd3352dd60fa8
MD Mehedi Hasan <mehedihasan.hr.324@gmail.com>
2025-03-01 16:36:07 +0600

    update

---
+++ src/app/(protected)/admin/dashboard/pages/events/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { EventsHeroSection } from "@/components";
+
+export default function EventPages() {
+  return (
+    <div className="p-6">
+      <EventsHeroSection />
+    </div>
+  );
+}
